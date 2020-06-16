19a615e0aa28f5fcb484d34b2b0d6b5b4b814ca8
Dickson Armijos <fabidick22@hotmail.es>
2020-06-15 22:15:27 -0500

    feat: update button

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment more and more',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
